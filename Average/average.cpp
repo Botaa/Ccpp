117229069e2a5917697ff1b5f9eb9eb4b4f2f779
Botaa <botaserban@gmail.com>
2018-10-28 16:36:26 +0200

    Average

---
+++ Average/average.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    double a,b;
+    double res;
+    cout<<"enter the first number: ";
+    cin>>a;
+    cout<<"enter the second number: ";
+    cin>>b;
+    res=(a+b)/2;
+    cout<<res<<endl;
+    return 0;
+}
